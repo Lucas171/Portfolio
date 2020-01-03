97df2346c98c83e1c4700c52a82340f52c977470
Lucas Miller <lucasmiller@Lucass-MBP.attlocal.net>
2020-01-03 02:16:00 -0600

    remove youtube link

--- index.js
+++
@@ -1,6 +0,0 @@
-$( ".top" ).scroll(function() {
-    if($(element).height() === "00px") {
-        // remove scroll fixed
-        $(scroll).css("position","relative") //or use absolute also.
-    }
-});
